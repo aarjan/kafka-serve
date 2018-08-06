39eb9cd333d1d6fdb993116d9e0026b6021a9f76
aarjan baskota <aarjan.baskota@dragonlaw.com.hk>
2018-08-06 17:03:34 +0545

    add debug for loaded schemas

--- kafka_producer/__init__.py
+++ kafka_producer/__init__.py
@@ -1,25 +1,29 @@
 import os
 import env_config
+from logger import log
 from confluent_kafka import avro
 from confluent_kafka.avro import AvroProducer
 from exceptions import ProducerException
 
+_SCHEMA_PATH = 'schema'
 schemas = {}
-for f_name in os.listdir('schema'):
-    with open('schema/'+f_name,'rU') as schema:
-        
+
+for f_name in os.listdir(_SCHEMA_PATH):
+    
+    with open(_SCHEMA_PATH + '/' + f_name,'r') as schema:
         name = f_name.split('.')[0]
-        
-        value_schema = avro.loads(schema.read())
 
         try:
-        
+            value_schema = avro.loads(schema.read())
+            
             schemas[name] =  AvroProducer(
                 {
-                "schema.registry.url":'http://localhost:8081',
-                "bootstrap.servers" : env_config.CONFIG["kafka_brokers"],
+                    "schema.registry.url":'http://localhost:8081',
+                    "bootstrap.servers" : env_config.CONFIG["kafka_brokers"],
                 },
                 default_value_schema=value_schema,
             )
+            log.debug("Loaded avro schema into schema directory",schema_name=name)
         except Exception as e:
+            print(name)
             raise ProducerException("could not initialize producer",e)
