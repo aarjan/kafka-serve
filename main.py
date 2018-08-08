720491cfaa5a6bc9d589020b0431b6d6e0ebc83a
aarjan baskota <aarjan.baskota@dragonlaw.com.hk>
2018-08-08 16:05:57 +0545

    add changes for running twisted server

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from logger import log
-from http.server import HTTPServer
-from http_server import RequestHandler
+import os
 import env_config
+from logger import log
+from twisted.web.server import Site
+from twisted.web.resource import Resource
+from twisted.internet import reactor, endpoints
+from http_handler import EventHandler
+from http_server import RequestHandler
+from http.server import HTTPServer
 
 LOG_FORMAT = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
 
@@ -19,17 +24,30 @@ def Main():
     print('Starting server at http://{}:{}'.format(host,port))
     log.info('server started',host=host,port=port)
     
-    RequestHandler.logger = log
-    server = HTTPServer((host,port),RequestHandler)
+    # RequestHandler.logger = log
+    # server = HTTPServer((host,port),RequestHandler)
 
+    # try:
+    #     server.serve_forever()
+    # except KeyboardInterrupt:
+    #     pass
+    # server.server_close()
+
+    root = Resource()
+    handler = EventHandler()
+    handler.logger = log
+    root.putChild(b"event", handler)
+    factory = Site(root)
+    endpoint = endpoints.TCP4ServerEndpoint(reactor, 8880)
+    endpoint.listen(factory)
     try:
-        server.serve_forever()
+        reactor.run()
     except KeyboardInterrupt:
         pass
-    server.server_close()
 
     log.info("server closed.")
     print('Server Closed.')
+    os._exit(1)
 
 if __name__ == "__main__" :
     Main() 
